cf47184135adb38bbf708a6da76be8f44416d786
KHAN-ALI-045 <161048750+KHAN-ALI-045@users.noreply.github.com>
2026-04-25 20:06:48 +0530

    Update transactions.js

--- transactions.js
+++ transactions.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const Transaction = require('../models/Transaction');
+const Transaction = require('./Transaction');
 
 // @desc    Get all transactions
 // @route   GET /api/transactions
